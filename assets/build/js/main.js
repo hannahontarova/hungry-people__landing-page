f656130cb93a4393fa9feae1cb5574367958ad3c
hannahontarova <anna08281207@gmail.com>
2021-07-06 20:16:46 +0300

    js bug, start edit scss code

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -21378,8 +21378,8 @@ return Popper;
 // 	// $('.input[name="phoneHead"]').mask('+38(099) 999-9999')
 // });
 
-const [...inputs] = document.querySelectorAll('#form input')
-const btnBook = document.getElementById(;)
+const [...inputs] = document.querySelectorAll('#form input select')
+const btnBook = document.getElementById('btnBook')
 let formData ={
     name: '',
     phone: '',
@@ -21408,36 +21408,35 @@ inputs[5].onclick = function (e){
     formData.time = e.target.value
 }
 
-
 btnBook.onclick = function(){
     if (!formData.name){
         inputs[0].style.borderColor = 'red'
     } else {
-        inputs[0].style.borderColor = '#616161'
+        inputs[0].style.borderColor = '#E5E5E5'
     }
     if (formData.phone.length === 9){
-        inputs[1].style.borderColor = '#616161'
+        inputs[1].style.borderColor = '#E5E5E5'
     } else {
         inputs[1].style.borderColor = 'red'
     }
     if (formData.date.length >= 3 && formData.date.length < 6){
-        inputs[2].style.borderColor = '#616161'
+        inputs[2].style.borderColor = '#E5E5E5'
     } else {
         inputs[2].style.borderColor = 'red'
     }
     if (formData.email.length >= 5){
-        inputs[3].style.borderColor = '#616161'
+        inputs[3].style.borderColor = '#E5E5E5'
     } else {
         inputs[3].style.borderColor = 'red'
     }
     if (!formData.people){
         inputs[4].style.borderColor = 'red'
     } else {
-        inputs[4].style.borderColor = '#616161'
+        inputs[4].style.borderColor = '#E5E5E5'
     }
     if (!formData.time){
         inputs[5].style.borderColor = 'red'
     } else {
-        inputs[5].style.borderColor = '#616161'
+        inputs[5].style.borderColor = '#E5E5E5'
     }
 }
